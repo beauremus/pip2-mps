b6bd6a5e7951f7447413215b3ef8e5d687264339
Beau Harrison <beau@fnal.gov>
2017-10-24 16:17:52 -0500

    Working without plots

---
+++ consts.js
@@ -0,0 +1,258 @@
+const requests = [
+  {
+    id: 'lmttyp',
+    device: 'P:LMTTYP',
+    output: 'text'
+  },
+  {
+    id: 'lmttyp',
+    device: 'P:LMTTYP.STATUS.READY',
+    output: 'status'
+  },
+  {
+    id: 'reset',
+    device: 'P:LMTTYP.STATUS.READY',
+    output: 'status'
+  },
+  {
+    id: 'lmtgw',
+    device: 'P:LMTGW',
+    output: 'text'
+  },
+  {
+    id: 'lmtwsz',
+    device: 'P:LMTWSZ',
+    output: 'text'
+  },
+  {
+    id: 'acrate',
+    device: 'P:ACRATE',
+    output: 'text'
+  },
+  {
+    id: 'lmtd12Set',
+    device: 'P:LMTD12.SETTING',
+    output: 'text'
+  },
+  {
+    id: 'lmtd12Status',
+    device: 'P:LMTD12.STATUS.ON',
+    output: 'status'
+  },
+  {
+    id: 'lmtd12Status',
+    device: 'P:LMTD12.STATUS.ON',
+    output: 'onOff'
+  },
+  {
+    id: 'lmtd1dSet',
+    device: 'P:LMTD1D.SETTING',
+    output: 'text'
+  },
+  {
+    id: 'lmtd1dStatus',
+    device: 'P:LMTD1D.STATUS.ON',
+    output: 'status'
+  },
+  {
+    id: 'lmtd1dStatus',
+    device: 'P:LMTD1D.STATUS.ON',
+    output: 'onOff'
+  },
+  {
+    id: 'lmtd2dSet',
+    device: 'P:LMTD2D.SETTING',
+    output: 'text'
+  },
+  {
+    id: 'lmtd2dStatus',
+    device: 'P:LMTD2D.STATUS.ON',
+    output: 'status'
+  },
+  {
+    id: 'lmtd2dStatus',
+    device: 'P:LMTD2D.STATUS.ON',
+    output: 'onOff'
+  },
+  {
+    id: 'lmtav10',
+    device: 'P:LMTAV1.SETTING[0]',
+    output: 'text'
+  },
+  {
+    id: 'lmtav11',
+    device: 'P:LMTAV1.SETTING[1]',
+    output: 'text'
+  },
+  {
+    id: 'lmtav12',
+    device: 'P:LMTAV1.SETTING[2]',
+    output: 'text'
+  },
+  {
+    id: 'lmtav13',
+    device: 'P:LMTAV1.SETTING[3]',
+    output: 'text'
+  },
+  {
+    id: 'lmtamx',
+    device: 'P:LMTAMX.SETTING',
+    output: 'text'
+  },
+  {
+    id: 'lmtav20',
+    device: 'P:LMTAV2.SETTING[0]',
+    output: 'text'
+  },
+  {
+    id: 'lmtav21',
+    device: 'P:LMTAV2.SETTING[1]',
+    output: 'text'
+  },
+  {
+    id: 'lmtav22',
+    device: 'P:LMTAV2.SETTING[2]',
+    output: 'text'
+  },
+  {
+    id: 'lmtav23',
+    device: 'P:LMTAV2.SETTING[3]',
+    output: 'text'
+  },
+  {
+    id: 'lmtsn1',
+    device: 'P:LMTSN1',
+    output: 'text'
+  },
+  {
+    id: 'lmtsn2',
+    device: 'P:LMTSN2',
+    output: 'text'
+  },
+  {
+    id: 'lmtsnd',
+    device: 'P:LMTSND',
+    output: 'text'
+  },
+  {
+    id: 'lmtd12',
+    device: 'P:LMTD12',
+    output: 'text'
+  },
+  {
+    id: 'lmtd1d',
+    device: 'P:LMTD1D',
+    output: 'text'
+  },
+  {
+    id: 'lmtd2d',
+    device: 'P:LMTD2D',
+    output: 'text'
+  },
+]
+
+const settings = [
+  {
+    id: 'reset',
+    device: 'P:LMTTYP',
+    eventType: 'click',
+    type: 'reset'
+  },
+  {
+    id: 'lmtd12Set',
+    device: 'P:LMTD12',
+    eventType: 'change',
+    type: 'set'
+  },
+  {
+    id: 'lmtd12Status',
+    device: 'P:LMTD12',
+    eventType: 'click',
+    type: 'toggle'
+  },
+  {
+    id: 'lmtd1dSet',
+    device: 'P:LMTD1D',
+    eventType: 'change',
+    type: 'set'
+  },
+  {
+    id: 'lmtd1dStatus',
+    device: 'P:LMTD1D',
+    eventType: 'click',
+    type: 'toggle'
+  },
+  {
+    id: 'lmtd2dSet',
+    device: 'P:LMTD2D',
+    eventType: 'change',
+    type: 'set'
+  },
+  {
+    id: 'lmtd2dStatus',
+    device: 'P:LMTD2D',
+    eventType: 'click',
+    type: 'toggle'
+  },
+  {
+    id: 'lmtav10',
+    device: 'P:LMTAV1[0]',
+    eventType: 'change',
+    type: 'set'
+  },
+  {
+    id: 'lmtav11',
+    device: 'P:LMTAV1[1]',
+    eventType: 'change',
+    type: 'set'
+  },
+  {
+    id: 'lmtav12',
+    device: 'P:LMTAV1[2]',
+    eventType: 'change',
+    type: 'set'
+  },
+  {
+    id: 'lmtav13',
+    device: 'P:LMTAV1[3]',
+    eventType: 'change',
+    type: 'set'
+  },
+  {
+    id: 'lmtav20',
+    device: 'P:LMTAV2[0]',
+    eventType: 'change',
+    type: 'set'
+  },
+  {
+    id: 'lmtav21',
+    device: 'P:LMTAV2[1]',
+    eventType: 'change',
+    type: 'set'
+  },
+  {
+    id: 'lmtav22',
+    device: 'P:LMTAV2[2]',
+    eventType: 'change',
+    type: 'set'
+  },
+  {
+    id: 'lmtav23',
+    device: 'P:LMTAV2[3]',
+    eventType: 'change',
+    type: 'set'
+  },
+  {
+    id: 'lmtamx',
+    device: 'P:LMTAMX',
+    eventType: 'change',
+    type: 'set'
+  }
+]
+
+const plots = [
+  {
+    id: '',
+    device: ''
+  }
+]
--- index.js
+++ index.js
@@ -1,136 +1,93 @@
 const dpm = new DPM()
 const acl = new ACLD()
-const lineWidth = 3
 
-// Use the margin convention practice
-const margin = {
-  top: 50,
-  right: 50,
-  bottom: 50,
-  left: 50
+function makeSetting(type) {
+  return (deviceName) => {
+    if (type === 'reset') {
+      return () => acl.run(`reset ${deviceName}`)
+    } else if (type === 'set') {
+      return (event) => acl.run(`set ${deviceName} ${event.target.value}`)
+    } else if (type === 'toggle') {
+      return (event) => {
+        if (event.target.value === 'ON') {
+          acl.run(`off ${deviceName}`)
+        } else if (event.target.value === 'OFF') {
+          acl.run(`on ${deviceName}`)
+        }
+      }
+    }
+  }
 }
-// Use the window's width
-const width = window.innerWidth - margin.left - margin.right
 
-// Use the window's height
-const height = window.innerHeight - margin.top - margin.bottom
+function output(type) {
+  return (id) => {
+    if (type === 'text') {
+      return (dpmData, dpmInfo) => {
+        let display = Number(dpmData.data)
 
-// The number of datapoints
-let n = 4000
+        if (isNaN(display)){
+          display = dpmData.data
+        } else {
+          display = `${display.toFixed(3)} ${dpmInfo.units}`
+        }
 
-// 6. Y scale will use the randomly generate number
-const yScale = d3.scaleLinear()
-  .domain([0, 80]) // input
-  .range([height, 0]) // output
+        document.querySelector(`#${id}`).textContent = display
+      }
+    } else if (type === 'status') {
+      return (dpmData, dpmInfo) => {
+        const element = document.querySelector(`#${id}`)
 
-const path = d3.line()
-  .x((d, i) => xScale(i)) // set the x values for the line generator
-  .y((d) => yScale(d)) // set the y values for the line generator
+        if (dpmData.data) {
+          element.setAttribute('style', 'background-color:lime')
+        } else {
+          element.setAttribute('style', 'background-color:red')
+        }
+      }
+    } else if (type === 'value') {
+      return () => {
+        console.error("Beau hasn't implemented value")
+      }
+    } else if (type === 'onOff') {
+      return (dpmData, dpmInfo) => {
+        if (dpmData.data) {
+          document.querySelector(`#${id}`).textContent = 'ON'
+        } else {
+          document.querySelector(`#${id}`).textContent = 'OFF'
+        }
+      }
+    }
+  }
+}
 
-// 5. X scale will use the index of our data
-const xScale = d3.scaleLinear()
-  .domain([0, n - 1]) // input
-  .range([0, width]) // output
+function plot(type) {
+  return (id) => {
+    return (dpmData, dpmInfo) => console.error("Beau hasn't implemented plots")
+  }
+}
 
-// An array of objects of length N. Each object has key -> value pair, the key being 'y' and the value is a random number
-let dataset = []
+for (setting of settings) {
+  const element = document.querySelector(`#${setting.id}`)
 
-// Add the SVG to the page and employ #2
-const svg = d3.select('#one')
-  .attr('width', width)
-  .attr('height', height)
-  .append('g')
-  .attr('transform', `translate(${margin.left}, ${margin.top})`)
+  element.addEventListener(
+    setting.eventType,
+    makeSetting(setting.type)(setting.device)
+  )
+}
 
-function bringToFront(input) {
-  if(typeof(input) === 'string') {
-    element = document.querySelector(input)
+for (request of requests) {
+  if (request.hasOwnProperty('bit')) {
+    dpm.addRequest(
+      request.device,
+      output(request.output)(request.id, {statusBit:request.bit})
+    )
   } else {
-    element = input
+    dpm.addRequest(request.device, output(request.output)(request.id))
   }
 
-  svg.node().appendChild(element)
 }
 
-function updateBars(data, info) {
-  const dataArray = new Float32Array(data.data, 4*5)
-  dataset = Array.from(dataArray)
-
-  svg.selectAll('.path').remove()
-
-  // Append the path, bind the data, and call the line generator
-  svg.append('path')
-    .datum(dataset) // Binds data to the line
-    .attr('class', 'path') // Assign a class for styling
-    .attr('d', path) // Calls the line generator
-
-  bringToFront('.threshold')
+for (plot of plots) {
+  console.log(plot)
 }
-
-function updateLine(line) {
-  return (dpmData, dpmInfo) => {
-    d3.select(line)
-      .attr('transform', `translate(0, ${yScale(dpmData.data)})`)
-
-    d3.select(line).select('.text').text(dpmData.data.toFixed(2))
-
-    const valueField = document.querySelector('#threshold .value')
-    valueField.textContent = dpmData.data
-  }
-}
-
-function moveLine() {
-  let dy = d3.event.sourceEvent.offsetY - margin.top
-
-  if (dy >= height) dy = height
-  if (dy <= lineWidth) dy = lineWidth
-
-  d3.select(this)
-    .attr('transform', `translate(0, ${dy})`)
-}
-
-function updateText() {
-  d3.select(this).select('.text')
-    .text(yScale.invert(d3.event.y).toFixed(2))
-}
-
-function dragStarted() {
-  d3.select(this).raise()
-  dpm.stop()
-}
-
-function dragged() {
-  moveLine.apply(this)
-  updateText.apply(this)
-}
-
-function dragStopped() {
-  acl.run(`set G:BEAU ${yScale.invert(d3.event.y)}`)
-  dpm.start()
-}
-
-const dragBehavior = d3.drag()
-  .on('start', dragStarted)
-  .on('drag', dragged)
-  .on('end', dragStopped)
-
-const threshold = svg.append('g')
-  .attr('class', 'threshold')
-  .attr('transform', `translate(0, ${height - margin.bottom - lineWidth})`)
-  .call(dragBehavior)
-
-const line = threshold.append('line')
-  .attr('stroke', 'red')
-  .attr('stroke-width', lineWidth)
-  .attr('x1', 0)
-  .attr('x2', width)
-
-const text = threshold.append('svg:text')
-  .attr('class', 'text')
-  .attr('dx', -10)
-  .attr('dy', -10)
-
-dpm.addRequest('G:BEAUARR[0:3999].RAW@P,1000', updateBars)
-dpm.addRequest('G:BEAU@P,67', updateLine('.threshold'))
 
 dpm.start()
